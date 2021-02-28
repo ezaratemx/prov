451f59a528dba27ece138ea038cfdc37785aa3da
ezaratemx <emzc80@gmail.com>
2021-02-28 02:27:46 -0600

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -16,3 +16,7 @@ systemctl enable telegraf
 #ip para privoxy
 direccion=$(zerotier-cli get af78bf9436b7ddf6 ip)
 sed -i "s/ipaddr/$direccion/g" /etc/privoxy/config
+apt install ufw
+sudo ufw enable
+ufw allow from 10.241.0.0/16 to any port 22
+ufw allow from 10.241.0.0/16 to any port 8118
