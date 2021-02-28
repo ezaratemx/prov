9bbdbbcdfcbae07d627d0fd1286eb087fd8b049e
ezaratemx <emzc80@gmail.com>
2021-02-28 02:42:10 -0600

    Update onboard.sh

--- onboard.sh
+++ onboard.sh
@@ -1,16 +1,1 @@
-#hostname
-sh -c 'cat /sys/class/net/eth0/address | sed s/://g > /etc/hostname'
-echo $(hostname -I | cut -d\  -f1) $(hostname) | tee -a /etc/hosts
 
-#zerotier
-curl -s https://install.zerotier.com | bash
-zerotier-cli join af78bf9436b7ddf6
-sleep 1m
-
-#ip para privoxy
-direccion=$(zerotier-cli get af78bf9436b7ddf6 ip)
-sed -i "s/ipaddr/$direccion/g" /etc/privoxy/config
-apt install ufw
-sudo ufw enable
-ufw allow from 10.241.0.0/16 to any port 22
-ufw allow from 10.241.0.0/16 to any port 8118
