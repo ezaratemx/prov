ac06c1ead35953481c0ad3a2dd2360f2954a916b
ezaratemx <emzc80@gmail.com>
2021-02-27 19:00:17 -0600

    Add files via upload

---
+++ 1.sh
@@ -0,0 +1,19 @@
+systemctl enable ssh
+systemctl start ssh
+apt update
+apt upgrade -y
+apt autoremove -y
+
+sh -c 'cat /sys/class/net/eth0/address | sed s/://g > /etc/hostname'
+
+# Set locale to en_US.UTF-8
+cp /etc/locale.gen /etc/locale.gen.dist
+sed -i -e "/^[^#]/s/^/#/" -e "/en_US.UTF-8/s/^#//" /etc/locale.gen
+
+cp /var/cache/debconf/config.dat /var/cache/debconf/config.dat.dist
+sed -i -e "/^Value: en_GB.UTF-8/s/en_GB/en_US/" \
+       -e "/^ locales = en_GB.UTF-8/s/en_GB/en_US/" /var/cache/debconf/config.dat
+locale-gen
+update-locale LANG=en_US.UTF-8
+wget https://chatt6.nyc3.digitaloceanspaces.com/2.sh
+reboot
---
+++ 2.sh
@@ -0,0 +1,31 @@
+# At this point, either log out and log in again, or reboot.
+# Rebooting seems easier if this is really being run from fabric.
+# If you do any upgrades, you may have to run the locale commands again
+
+# Set timezone to America/Mexico_City
+#cp /etc/timezone /etc/timezone.dist
+wget https://chatt6.nyc3.digitaloceanspaces.com/timezone
+rm etc/timezone
+mv timezone /etc/timezone
+#echo "America/Mexico_City" > /etc/timezone
+dpkg-reconfigure -f noninteractive tzdata
+
+# Set the keyboard to US, don't set any modifier keys, etc.
+#cp /etc/default/keyboard /etc/default/keyboard.dist
+#sed -i -e "/XKBLAYOUT=/s/gb/us/" /etc/default/keyboard
+#service keyboard-setup restart
+
+#zerotier
+curl -s https://install.zerotier.com | bash
+
+#privoxy
+apt install -y privoxy
+
+#telegraf
+curl -sL https://repos.influxdata.com/influxdb.key | apt-key add -
+echo "deb https://repos.influxdata.com/debian buster stable" | tee /etc/apt/sources.list.d/influxdb.list
+
+apt-get update
+apt-get install -y telegraf
+systemctl enable telegraf
+#systemctl start telegraf
